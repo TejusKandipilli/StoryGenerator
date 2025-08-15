a6c39973b959778fe8ad352e19d4cef9b0169de6
Tejus Kandipilli <tejusking12447@gmail.com>
2025-08-15 13:49:45 +0530

    minor fix app

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ graph_continue = graph_builder_continue.compile()
 app = FastAPI(title="AI Story Generator with LangGraph + Gemini")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://story-generator-murex.vercel.app/","http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"],
+    allow_origins=["https://story-generator-murex.vercel.app","http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
